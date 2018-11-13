631a5865dd2b2912d590543d0be06ce1d935722a
Gość <belicja1@gmail.com>
2018-11-13 11:38:01 +0100

    added delete mechanism

---
+++ zad/delete.php
@@ -0,0 +1,5 @@
+<?php
+    require("../lib/connect.php");
+
+    echo($_POST["deleteid"]);
+?>
--- zad/index.php
+++ zad/index.php
@@ -16,13 +16,22 @@
         <section>
             <article>
                 <?php
+                    $sql = "select * from pracownicy;";
+                    $result = $conn->query($sql);
+                    
+                    echo("<table>");
+                    while($row = $result->fetch_assoc()) {
+                        echo("<tr><td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>"."<form action='delete.php' method='POST'><input class='invisible' type='text' name='deleteid' value='".$row["id_pracownicy"]."'><input type='submit' value='usun'></td></tr>");
+                    }
+                    echo("</table>");
+
                     if(count($_POST) == 0) {
                         echo("Nie wysłano zapytania");
                     }
                     else {
                         $sql = "select year(curdate()) - year('".$_POST["date"]."') as wiek;";
-                        $result = mysqli_query($conn, $sql);
-                        $row = mysqli_fetch_assoc($result);
+                        $result = $conn->query($sql);
+                        $row = $result->fetch_assoc();
 
                         echo("<h3>Zadanie 1</h3>");
                         echo("<li>Dane: ".$_POST["date"]);
@@ -31,12 +40,13 @@
                         echo("<li>Ilość miesięcy: ".($row["wiek"] * 12));
 
                         $sql = "select datediff(curdate(), '".$_POST["date"]."') as dni;";
-                        $result = mysqli_query($conn, $sql);
-                        $row = mysqli_fetch_assoc($result);
+                        $result = $conn->query($sql);
+                        $row = $result->fetch_assoc();
 
                         echo("<li>Ilość tygodni: ".(int)($row["dni"] / 7));
                         echo("<li>Ilość dni: ".($row["dni"]));
                         echo("<li>Ilość sekund: ".($row["dni"] * 24 * 60 * 60));
+                        $conn->close();
                     }
                 ?>
             </article>
