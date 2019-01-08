95e720a8aa64848c8c45f89ab96ffa30072e2938
Gość <belicja1@gmail.com>
2019-01-08 12:15:28 +0100

    read from field

--- hhh/script.js
+++ hhh/script.js
@@ -1,9 +1,10 @@
 const mainDiv = document.querySelector('.mainDiv');
 const guzik = document.querySelector('.button');
+const input = document.querySelector('.tekst');
 
 guzik.addEventListener('click', function(){
     const dzieckoDiv = document.createElement('div');
     dzieckoDiv.className = "fajna_klasa";
-    dzieckoDiv.innerText = "tekst w divie o nazwie dzieckoDiv";
+    dzieckoDiv.innerText = input.value;
     mainDiv.appendChild(dzieckoDiv);
 });
