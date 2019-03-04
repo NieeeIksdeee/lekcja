8a85a0bbbd0fedaa48f852e01e3af20272d6b365
Gość <grzelakm10@gmail.com>
2019-03-04 14:04:10 +0100

    x

--- Marzec4/main.js
+++ Marzec4/main.js
@@ -1,5 +1,6 @@
 const btnGet = document.querySelector(".btnget");
 const list = document.querySelector(".results");
+const btnSend = document.querySelector(".btnsend");
 
 btnGet.addEventListener("click", function() {
     let xhr = new XMLHttpRequest();
@@ -15,5 +16,18 @@ btnGet.addEventListener("click", function() {
                 list.appendChild(li);
             })
         }
+        else {
+            console.log("Stan połączenia: " + this.status);
+        }
     });
+    xhr.addEventListener("error", function() {
+        console.log("Brak połączenia z serwerem");
+    })
+});
+
+btnSend.addEventListener("click", function() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://172.16.131.125/3ti/grzelak_m/Marzec4/dane.php", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send("imie="+imie+" & nazwisko="+nazwisko);
 });
